d2f0331ff17cd06699ebee290fa54c03ac81b3e3
MrAlex6204 <engineer.vera@yahoo.com>
2016-08-25 10:11:21 -0500

    Se hiso el fix para porder hacer la compilacion correctamente y se refactorizo algunos metodos

--- Dev PuntoDeVentas/PuntoDeVentas/Program.cs
+++ Dev PuntoDeVentas/PuntoDeVentas/Program.cs
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,69 +42,3 @@ namespace PuntoDeVentas
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
-
-namespace PuntoDeVentas
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);            
-            //Application.Run(new FRM_Login());
-            FRM_Login wndLogin = new FRM_Login();
-            FRM_VentanaPrincipal wndVentanaPrinicipal = new FRM_VentanaPrincipal();
-            FRM_Menu wndMenu = new FRM_Menu();
-            FRM_SplashScreen wndLoading = new FRM_SplashScreen();
-            wndLoading.ShowDialog();
-
-            wndVentanaPrinicipal.Show();
-            wndLogin.ShowDialog(wndVentanaPrinicipal);
-            wndMenu.ShowDialog();
-            Application.Run();            
-        }
-    }
-}
->>>>>>> 208d285bee0af146f9f98d8836b287fa0df48b6b
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
-
-namespace PuntoDeVentas
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);            
-            //Application.Run(new FRM_Login());
-            FRM_Login wndLogin = new FRM_Login();
-            FRM_VentanaPrincipal wndVentanaPrinicipal = new FRM_VentanaPrincipal();
-            FRM_Menu wndMenu = new FRM_Menu();
-            FRM_SplashScreen wndLoading = new FRM_SplashScreen();
-            wndLoading.ShowDialog();
-
-            wndVentanaPrinicipal.Show();
-            wndLogin.ShowDialog(wndVentanaPrinicipal);
-            wndMenu.ShowDialog();
-            Application.Run();            
-        }
-    }
-}
->>>>>>> 208d285bee0af146f9f98d8836b287fa0df48b6b
